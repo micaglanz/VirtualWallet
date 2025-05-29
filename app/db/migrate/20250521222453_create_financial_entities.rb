4113a48c979081085dc0ba47e3c1873b6176ac09
MatiasV92 <matiasvicharelli.92@gmail.com>
2025-05-29 13:52:58 -0300

    mybranch update

---
+++ app/db/migrate/20250521222453_create_financial_entities.rb
@@ -0,0 +1,10 @@
+class CreateFinancialEntities < ActiveRecord::Migration[8.0]
+  def change
+    create_table :financial_entities do |t|
+      t.string :name, null: false
+      t.string :entity_type
+      t.string :currency
+      t.timestamps
+    end
+  end
+end
